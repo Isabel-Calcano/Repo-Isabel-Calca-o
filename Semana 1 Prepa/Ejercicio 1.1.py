019a0a0c11b12931aef69bf0d015c72514b00a51
Isabel-Calcano <calcano.isabel@correo.unimet.edu.ve>
2022-09-16 10:15:56 -0400

    commit 3

---
+++ Semana 1 Prepa/Ejercicio 1.1.py
@@ -0,0 +1,7 @@
+saldo = 3480
+print(f"Saldo inicial: $ {saldo}")
+# Se hace una compra de $96 en una tienda
+fondo_retirado = 96
+print("se han retirado $96")
+# Mostrar saldo actual
+print(f"Saldo actual a la fecha (2020/04/11): ${saldo - fondo_retirado}")
